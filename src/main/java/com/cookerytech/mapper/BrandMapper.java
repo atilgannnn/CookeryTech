0ee9142fad809459a2fb1f5840ce411f518bb936
Fatmatrn <ftmtrn3488@gmail.com>
2023-08-17 14:28:36 +0300

    brand_update_and_delete

--- src/main/java/com/cookerytech/mapper/BrandMapper.java
+++ src/main/java/com/cookerytech/mapper/BrandMapper.java
@@ -4,9 +4,16 @@ import com.cookerytech.domain.Brand;
 import com.cookerytech.dto.BrandDTO;
 import com.cookerytech.dto.request.BrandSaveRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
+@Component
 public interface BrandMapper {
+
+
+    BrandDTO brandToBrandDTO(Brand brand);
+
 
     Brand brandSaveRequestToBrand(BrandSaveRequest brandSaveRequest);
 }
