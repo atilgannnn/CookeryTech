3f85d7f6afbb149cd0f01071765a88e28f80fb5a
atilgan <a.sahinulasan@gmail.com>
2023-08-20 04:57:08 +0300

    Atılgan C01-C02

--- src/main/java/com/cookerytech/repository/BrandRepository.java
+++ src/main/java/com/cookerytech/repository/BrandRepository.java
@@ -1,7 +1,12 @@
 package com.cookerytech.repository;
 
 import com.cookerytech.domain.Brand;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+    @Query("select * from Brand")
+    Page<Brand> getActiveBrands(Pageable pageable);//TODO - İki methodun da aynı Query'yi kullanması doğru mu?
 }
