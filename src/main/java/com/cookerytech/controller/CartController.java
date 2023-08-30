449bfdbe2c442d936a19a40b37701ccf4d102cfb
Yunus <yunus.can42320@gmail.com>
2023-08-30 03:01:38 +0200

    D01-D02 shopping cart

--- src/main/java/com/cookerytech/controller/CartController.java
+++ src/main/java/com/cookerytech/controller/CartController.java
@@ -1,15 +1,18 @@
 package com.cookerytech.controller;
 
+import com.cookerytech.domain.Cart;
+import com.cookerytech.domain.User;
+import com.cookerytech.dto.request.CartItemRequest;
 import com.cookerytech.dto.response.CartResponse;
 import com.cookerytech.service.CartService;
 import com.cookerytech.service.UserService;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cart")
@@ -19,18 +22,27 @@ public class CartController {
 
     private final CartService cartService;
 
-    public CartController(UserService userService, CartService cartService) {
+    public CartController(UserService userService,@Lazy CartService cartService) {
         this.userService = userService;
         this.cartService = cartService;
     }
 
     @GetMapping("/auth")
-    @PreAuthorize("hasRole('ADMIN') or hasRole('SALES_SPECIALIST') or hasRole('SALES_MANAGER') or " +
-            " hasRole('PRODUCT_MANAGER') or hasRole('CUSTOMER')")
     public ResponseEntity<List<CartResponse>> getUsersCart(){
+
         List<CartResponse> cart = cartService.getCart();
 
         return ResponseEntity.ok(cart);
+    }
+
+    @PostMapping("/auth")
+    public ResponseEntity<CartResponse> manageCartItem(@RequestBody CartItemRequest cartItemRequest) {
+        CartResponse cartResponse = cartService.manageCartItem(
+                cartItemRequest.getModelId(),
+                cartItemRequest.getAmount()
+        );
+
+        return ResponseEntity.ok(cartResponse);
     }
 
 }
--- src/main/java/com/cookerytech/repository/CartRepository.java
+++ src/main/java/com/cookerytech/repository/CartRepository.java
@@ -1,7 +1,6 @@
 package com.cookerytech.repository;
 
 import com.cookerytech.domain.Cart;
-import com.cookerytech.domain.Cart_Items;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,6 +9,9 @@ import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart,Long> {
+
+//    @Query("SELECT c FROM Cart c JOIN c.user u WHERE u.id = :id")
+//    Cart getCartsWithUserId(@Param("id") Long id);
 
     @Query("SELECT c FROM Cart c")
     List<Cart> findAllCarts();
